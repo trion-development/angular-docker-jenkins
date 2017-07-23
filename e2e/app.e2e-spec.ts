6aac47197c0df4c566d87dbc2a737f1d66ea8b5a
Thomas Kruse <tk@trion.de>
2017-07-23 19:13:53 +0200

    update

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('heroku-demo App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works on heroku!');
+    expect(page.getParagraphText()).toEqual('app works on docker!');
   });
 });
