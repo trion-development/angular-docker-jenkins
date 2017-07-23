6cbc102e4f628ed25051e4b4a25142f218c10148
Thomas Kruse <tk@trion.de>
2017-07-23 19:48:04 +0200

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('app works on docker!');
   }));
 });
